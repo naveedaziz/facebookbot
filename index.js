ba63efc97af2f050ac8ffda6fbcfb12f57de6506
naveedaziz <nidobda_92@hotmail.com>
2016-12-15 15:09:31 +0500

    ded

--- index.js
+++ index.js
@@ -78,53 +78,53 @@ function sendGenericMessage(sender) {
 		url: 'http://fishry-beta.azure-mobile.net/api/collection_request?store_id=619708C1-32D9-45CC-A9D7-51E23D5EB4FA',
 		method: 'GET',
 	}, function(error, response, body) {
-		if(typeof(body) != 'object'){
-			var datas = JSON.parse(body);
-		}else{
-			var datas = body;
-		}
-		var collections  = [];
-		for(var dts in data){
-			collections.push({
-							"title": data[dts].collectionName,
-							"subtitle": data[dts].collectionName,
-							"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-							"buttons": [{
-								"type": "web_url",
-								"url": "https://www.messenger.com",
-								"title": data[dts].collectionName
-							}, {
-								"type": "postback",
-								"title": "Postback",
-								"payload": data[dts].collectionName,
-							}],
-						});
-		}
-				var  messageData = {
-				"attachment": {
-					"type": "template",
-					"payload": {
-						"template_type": "generic",
-						"elements": [collections]
-					}
-				}
+	   	var messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
 			}
-			request({
-				url: 'https://graph.facebook.com/v2.6/me/messages',
-				qs: {access_token:token},
-				method: 'POST',
-				json: {
-					recipient: {id:sender},
-					message: messageData,
-				}
-			}, function(error, response, body) {
-				if (error) {
-					console.log('Error sending messages: ', error)
-				} else if (response.body.error) {
-					console.log('Error: ', response.body.error)
-				}
-			})
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
 	})
+	   })
 	
 }
 
