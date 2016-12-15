4226c86217f722c1c4ab9fe79404bd9dfa91fcfc
naveedaziz <nidobda_92@hotmail.com>
2016-12-15 16:27:01 +0500

    ded

--- index.js
+++ index.js
@@ -75,52 +75,64 @@ function sendTextMessage(sender, text) {
 }
 
 function sendGenericMessage(sender) {
-	var messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
 	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
+		url: 'https://fishry-beta.azure-mobile.net/api/collection_request?store_id=619708C1-32D9-45CC-A9D7-51E23D5EB4FA',
 		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
 	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
+		if(body){
+			if(typeof(body) != 'object'){
+				body = JSON.parse(body);
+			}
+			var collections = body;
+			var cols = [];
+			for(var bd in body){
+				if(body[bd] && body[bd].collectionName){
+					cols.push({
+									"title": body[bd].collectionName,
+									"subtitle": body[bd].collectionName,
+									"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+									"buttons": [{
+										"type": "web_url",
+										"url": "https://www.messenger.com",
+										"title": "web url"
+									}, {
+										"type": "postback",
+										"title": "Postback",
+										"payload": body[bd].collectionName,
+									}],
+								});
+				}
+				
+			}
+			var messageData = {
+						"attachment": {
+							"type": "template",
+							"payload": {
+								"template_type": "generic",
+								"elements": cols
+							}
+						}
+					}
+					request({
+						url: 'https://graph.facebook.com/v2.6/me/messages',
+						qs: {access_token:token},
+						method: 'POST',
+						json: {
+							recipient: {id:sender},
+							message: messageData,
+						}
+					}, function(error, response, body) {
+						if (error) {
+							console.log('Error sending messages: ', error)
+						} else if (response.body.error) {
+							console.log('Error: ', response.body.error)
+						}
+					})
+		}else{
 		}
+						
 	})
+	
 }
 
 // spin spin sugar
