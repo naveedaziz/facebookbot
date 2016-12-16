3484cd42a1cb3db72d6f613e932e40b0ed7bc8ba
naveedaziz <nidobda_92@hotmail.com>
2016-12-16 12:53:48 +0500

    deD

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
  
 db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
+  db.run("CREATE TABLE users (sender TEXT,recipient TEXT,cart TEXT,product_selected TEXT,store_id TEXT,dated TEXT)");
  
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
+  var stmt = db.run("INSERT INTO users (sender, recipient, cart) VALUES ('1', '2', '3')");
  
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
+  db.each("SELECT rowid AS id, sender FROM users", function(err, row) {
+      console.log(row.id + ": " + row.sender);
   });
 });
  
